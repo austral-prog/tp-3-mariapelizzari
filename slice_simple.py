fffa90d3f660550a31f400231f5e0d5b079ec78d
mariapelizzari <mpelizzari@mail.austral.edu.ar>
2025-04-04 18:47:40 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,8 +1,8 @@
 def slice_simple():
     texto = "Awesome"
-	comienzo = texto[1:4].lower()
-	medio = texto[2:5].lower()
-	todo = texto[ : ].lower()
-	print(comienzo)
-	print(medio)
-	print(todo)
+    comienzo = texto[1:4].lower()
+    medio = texto[2:5].lower()
+    todo = texto[ : ].lower()
+    print(comienzo)
+    print(medio)
+    print(todo)
