5902f87cea1318624e9449d9090eb38cd7e73ca5
mariapelizzari <mpelizzari@mail.austral.edu.ar>
2025-04-04 18:50:23 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,3 @@
 def slice_advanced():
-	texto = "Awesome"
-
-	comienzo = texto[1:4].lower()
-	medio = texto[2:5].lower()
-	todo = texto[ : ].lower()
-	print(comienzo)
-	print(medio)
-	print(todo)
+	texto = input("Ingrese un texto: ")
+	print(texto[4::2])
